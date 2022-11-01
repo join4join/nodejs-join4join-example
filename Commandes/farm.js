08bce1d8fb9569b2fef44e2a56d5f150c6fffee2
grality <leodiscord41@gmail.com>
2022-11-01 21:02:22 +0100

    update files

--- Commandes/farm.js
+++ Commandes/farm.js
@@ -33,17 +33,16 @@ module.exports = {
                 const guild_4 = await info(farm_.data[3])
                 const guild_5 = await info(farm_.data[4])
 
-
                 const emb = new Discord.EmbedBuilder()
                     .setColor(message.member.displayHexColor)
                     .setTitle('join4join.xyz | Farm')
                     .setURL(config.url)
                     .setDescription(`**Join these servers to get 0.75 coins each.**\n**Use the coins to buy members on the website:** [https://join4join.xyz](https://join4join.xyz)\n**Leaving a server before 4 days past will deduct 2 coins from your balance**\n\n
-                \`${guild_1.name}\`\n> [Click here to join](${guild_1.invite})\n> **Order: ${guild_1.invitation_update}/${guild_1.invitation_request}**\n
-                \`${guild_2.name}\`\n> [Click here to join](${guild_2.invite})\n> **Order: ${guild_2.invitation_update}/${guild_2.invitation_request}**\n
-                \`${guild_3.name}\`\n> [Click here to join](${guild_3.invite})\n> **Order: ${guild_3.invitation_update}/${guild_3.invitation_request}**\n
-                \`${guild_4.name}\`\n> [Click here to join](${guild_4.invite})\n> **Order: ${guild_4.invitation_update}/${guild_4.invitation_request}**\n
-                \`${guild_5.name}\`\n> [Click here to join](${guild_5.invite})\n> **Order: ${guild_5.invitation_update}/${guild_5.invitation_request}**
+                \`${guild_1.data.name}\`\n> [Click here to join](${guild_1.data.invite})\n> **Order: ${guild_1.data.invitation_update}/${guild_1.data.invitation_request}**\n
+                \`${guild_2.data.name}\`\n> [Click here to join](${guild_2.data.invite})\n> **Order: ${guild_2.data.invitation_update}/${guild_2.data.invitation_request}**\n
+                \`${guild_3.data.name}\`\n> [Click here to join](${guild_3.data.invite})\n> **Order: ${guild_3.data.invitation_update}/${guild_3.data.invitation_request}**\n
+                \`${guild_4.data.name}\`\n> [Click here to join](${guild_4.data.invite})\n> **Order: ${guild_4.data.invitation_update}/${guild_4.data.invitation_request}**\n
+                \`${guild_5.data.name}\`\n> [Click here to join](${guild_5.data.invite})\n> **Order: ${guild_5.data.invitation_update}/${guild_5.data.invitation_request}**
                 \n\n**After joining some servers [click here](https://join4join.xyz/claim) to get your coins.**`)
                     .setThumbnail(bot.user.displayAvatarURL())
                     .setFooter({ text: config.footer })

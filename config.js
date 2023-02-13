6c0b9f0ab433094f4a1f7920b4757b90b122ab87
Lolwdecent <113751993+Lolwdecent@users.noreply.github.com>
2023-02-13 09:56:22 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: "YOUR BOT TOKEN",
-    bot_invite_link: "YOUR BOT INVITE LINK",
+    token: "MTA3NDU0MjczNjU4NzM1ODI4OA.GTvSBM.JzsKP1oXLemXuBhGfNfUii4HuWh99Vz8VW8RzQ",
+    bot_invite_link: "https://discord.com/api/oauth2/authorize?client_id=1074542736587358288&permissions=8&scope=bot",
     url: "https://join4join.xyz",
-    footer: "discord.gg/join4join",
+    footer: "https://discord.gg/RHS7UVwZ",
     color: "#e84e4b",
-    admin: [""],
-    owner: [""],
-    api_key: "YOUR API KEY (https://join4join.xyz/profile)",
+    admin: ["811771148613189652"],
+    owner: ["811771148613189652"],
+    api_key: "YOUR API KEY (7cceabb0230545ea942016b8f28d5fbc)",
     api_url: "https://join4join.xyz",
 }

fdd9906ae2b15ab8ab3ba648fbfc4fc66427156f
Bassvoll2401 <61481235+Bassvoll2401@users.noreply.github.com>
2023-03-23 10:33:10 +0000

    Pending changes exported from your codespace

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: "YOUR BOT TOKEN",
-    bot_invite_link: "YOUR BOT INVITE LINK",
+    token: "MTA4ODQwNTg4NDI5NzgyMjI2OQ.GGoiHB.JOgQzbXX7kxf1eUkl1yGbh1rzoCJp1aELqpWjs",
+    bot_invite_link: "https://discord.com/api/oauth2/authorize?client_id=1088405884297822269&permissions=2049&scope=bot",
     url: "https://join4join.xyz",
-    footer: "discord.gg/join4join",
+    footer: "https://discord.gg/freeadvertisement",
     color: "#e84e4b",
-    admin: [""],
-    owner: [""],
-    api_key: "YOUR API KEY (https://join4join.xyz/profile)",
+    admin: ["Nassvoll"],
+    owner: ["Nassvoll"],
+    api_key: "5fd9adfa21ac4d9892c6a1facccb7075",
     api_url: "https://join4join.xyz",
 }

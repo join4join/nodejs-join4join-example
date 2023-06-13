d9b95c81eafa8724d2c24e437e444dfca43e1b7a
grality <leodiscord41@gmail.com>
2023-06-13 11:33:09 +0200

    remove useless intents

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const Discord = require("discord.js")
-const intents = new Discord.IntentsBitField(3276799)
-const bot = new Discord.Client({intents})
-const loadCommands = require("./Loaders/loadCommands")
-const loadEvents = require("./Loaders/loadEvents")
-const config = require("./config")
+const bot = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+    ],
+});
 
-bot.commands = new Discord.Collection()
+const loadCommands = require("./Loaders/loadCommands");
+const loadEvents = require("./Loaders/loadEvents");
+const config = require("./config");
 
-bot.login(config.token)
-loadCommands(bot)
-loadEvents(bot)
+bot.commands = new Discord.Collection();
+
+bot.login(config.token);
+loadCommands(bot);
+loadEvents(bot);
